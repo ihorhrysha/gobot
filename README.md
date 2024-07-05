2d622cc99d5aecde5713ba059ed262cab6ddc73e
Ihor Hrysha <ihor.hrysha@gmail.com>
2024-07-05 14:18:47 +0300

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# 🤖 gobot
+
+
+## Build project 
+
+```bash
+go build -ldflags "-X="github.com/ihorhrysha/gobot/cmd.version=0.1.2
+```
+
+## Bot
+
+t.me/ihorhrysha_go_bot
+
+## CLI
+```
+A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.
+
+Usage:
+  gobot [command]
+
+Available Commands:
+  completion  Generate the autocompletion script for the specified shell
+  gobot       A brief description of your command
+  help        Help about any command
+  version     A brief description of your command
+
+Flags:
+  -h, --help     help for gobot
+  -t, --toggle   Help message for toggle
+
+Use "gobot [command] --help" for more information about a command.
+```
---
+++ cmd/gobot.go
@@ -0,0 +1,61 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	tele "gopkg.in/telebot.v3"
+)
+
+var TeleToken = os.Getenv("TELE_TOKEN")
+
+// gobotCmd represents the gobot command
+var gobotCmd = &cobra.Command{
+	Use:     "gobot",
+	Aliases: []string{"start", "run", "go"},
+	Short:   "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("gobot v%s started", version)
+		bot, err := tele.NewBot(tele.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		bot.Handle(tele.OnText, func(c tele.Context) error {
+			return c.Reply("Hello, " + c.Sender().FirstName + "!" + " I'm a bot!" + "\nYou've asked me: " + c.Text())
+		})
+
+		bot.Start()
+
+		fmt.Printf("gobot v%s stopped", version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(gobotCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// gobotCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// gobotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
